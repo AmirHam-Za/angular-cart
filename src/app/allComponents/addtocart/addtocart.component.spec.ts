fbfbf5709b314d0e2d03eeda31851c604d9b6fb0
Amir Hamza <hamza.com.bd@gmail.com>
2024-02-20 23:20:21 +0600

    addtocart compoment added

--- src/app/allComponents/addtocart/addtocart.component.spec.ts
+++ src/app/allComponents/addtocart/addtocart.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AddtocartComponent } from './addtocart.component';
+import { AddToCartButtonComponent } from './addtocart.component';
 
 describe('AddtocartComponent', () => {
-  let component: AddtocartComponent;
-  let fixture: ComponentFixture<AddtocartComponent>;
+  let component: AddToCartButtonComponent;
+  let fixture: ComponentFixture<AddToCartButtonComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AddtocartComponent]
+      imports: [AddToCartButtonComponent]
     })
     .compileComponents();
     
-    fixture = TestBed.createComponent(AddtocartComponent);
+    fixture = TestBed.createComponent(AddToCartButtonComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
